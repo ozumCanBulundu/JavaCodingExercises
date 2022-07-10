434403ab0a22c55edcb5c65194b4f93f3692e792
ozumCanBulundu <ozumcanbulundu@gmail.com>
2022-07-10 21:28:28 +0300

    Initial Commit

---
+++ PoolArea/src/Cuboid.java
@@ -0,0 +1,18 @@
+
+public class Cuboid extends Rectangle{
+
+    private double height;
+
+    public Cuboid(double width, double lenght, double height){
+        super(width, lenght);
+        this.height = height < 0 ? 0 : height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume(){
+        return getArea()*height;
+    }
+}
