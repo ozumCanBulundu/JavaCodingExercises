f49be212b96c433e640ce863cb4903a787f02bd9
ozumCanBulundu <ozumcanbulundu@gmail.com>
2022-08-18 10:22:14 +0300

    finished

--- Bills Burgers/src/DeluxeBurger.java
+++ Bills Burgers/src/DeluxeBurger.java
@@ -1,3 +1,28 @@
-public class DeluxeBurger {
+public class DeluxeBurger extends Hamburger{
 
+    public DeluxeBurger() {
+        super("Deluxe","Bacon & Cheese","Sesame Bun", 19.10);
+        super.addHamburgerAddition1("Chips", 0);
+        super.addHamburgerAddition2("Drink", 0);
+    }
+
+    @Override
+    public void addHamburgerAddition1(String name,double price){
+        System.out.println("No additional items can be added to a deluxe burger.");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("No additional items can be added to a deluxe burger.");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("No additional items can be added to a deluxe burger.");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("No additional items can be added to a deluxe burger.");
+    }
 }
--- Bills Burgers/src/Hamburger.java
+++ Bills Burgers/src/Hamburger.java
@@ -76,11 +76,48 @@ public class Hamburger {
     private String breadRollType;
     private double price;
 
+
+    private String addition1Name;
+    private  double addition1Price;
+    private String addition2Name;
+    private double addition2Price;
+    private String addition3Name;
+    private  double addition3Price;
+    private String addition4Name;
+    private  double addition4Price;
+
+
+
     public Hamburger(String name,String meat,String breadRollType,double price){
 
         this.name = name;
         this.meat = meat;
         this.breadRollType = breadRollType;
         this.price = price;
+    }
+
+    public void addHamburgerAddition1(String name,double price){
+        this.price += price;
+        addition1Name = name;
+    }
+
+    public void addHamburgerAddition2(String name,double price){
+        this.price += price;
+        addition2Name = name;
+    }
+
+    public void addHamburgerAddition3(String name,double price){
+        this.price += price;
+        addition3Name = name;
+    }
+
+    public void addHamburgerAddition4(String name,double price){
+        this.price += price;
+        addition4Name = name;
+    }
+
+    public double itemizeHamburger() {
+        System.out.println("One order of " + name + " on " + breadRollType + " with " + addition1Name + " " + addition2Name + " " + addition3Name + " " + addition4Name);
+        return (price + addition1Price + addition2Price + addition3Price + addition4Price);
     }
 }
